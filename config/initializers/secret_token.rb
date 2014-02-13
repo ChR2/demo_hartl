ce76dfa8b22452d7c7f724ddae0d539f149e02fe
Wendy Sanchez-Vaynshteyn <WendyGSV@gmail.com>
2014-02-12 19:09:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoHartl::Application.config.secret_key_base = 'd9ea01af84c01862431fa6a041c3771e53fd9355b12727141d6b3e3700505cf622f4b4e6e1014916c3a383427e6188723e03ab528ae571841aca4c0a5577286d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoHartl::Application.config.session_store :cookie_store, key: '_demo_hartl_session'
